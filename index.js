6e459a85ac0b2ab5364ad17f07b2bfca6b20682b
Ishidres <ishidres@gmail.com>
2020-01-10 14:59:15 +0100

    finished v1.0.0

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const fs = require("fs");
-const config = require("config.json");
-const package = require("package.json");
+const config = require("./config.json");
+const package = require("./package.json");
 
 var saved_users = {};
 
 // import savedUsers.json if it exists already
 if (fs.existsSync(__dirname + "/savedUsers.json"))
-  saved_users = JSON.parse("./savedUsers.json");
+  saved_users = JSON.parse(fs.readFileSync("./savedUsers.json", "utf8"));
 
 client.on('ready', () => {
   console.log(new Date() + ": Logged in as " + client.user.tag);
@@ -20,15 +20,21 @@ client.on('message', message => {
   if (message.author.bot) return;
 
   // reply when receiving !help in direct message
-  if (message.channel.type === "dm" && !!message.content) {
-    if (message.content.toLowerCase().startsWith("!help"))
-      return message.reply("This bot doesn't offer any commands and has to be set up manually. Please refer to " + package.homepage + " for more information.");
+  if (message.channel.type === "dm") {
+    if (!!message.content) {
+      if (message.content.toLowerCase().startsWith("!help"))
+        message.reply("This bot doesn't offer any commands and has to be set up manually."
+        + "\nPlease refer to " + package.homepage + " for more information.");
+    }
+
+    return;
   }
 
   if (!saved_users[message.author.id])
     saved_users[message.author.id] = {};
 
   saved_users[message.author.id].lastMessage = new Date().getTime();
+  saved_users[message.author.id].guild = message.guild.id;
   console.log(new Date() + ": Message received by " + message.author.tag);
 
   // Save the data
@@ -37,9 +43,21 @@ client.on('message', message => {
 
 // checking for users to kick every 10 minutes (600 000 ms)
 setInterval(function () {
-  Object.keys(saved_users).map(function () {
-    // TODO
+  var now = new Date().getTime();
+
+  Object.keys(saved_users).map(function (i) {
+    if (now - saved_users[i].lastMessage >= config.maxInactivity) {
+      console.log(new Date() + ": Kicking User " + i + " due too inactivity.");
+      var guild = client.guilds.get(saved_users[i].guild);
+
+      guild.fetchMember(i).then(member => {
+        member.kick("Kicked due too inactivity.");
+      });
+
+      delete saved_users[i];
+      fs.writeFileSync("./savedUsers.json", JSON.stringify(saved_users));
+    }
   });
-}, 600000)
+}, 60*1000);
 
 client.login(config.token);
