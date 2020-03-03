654807422b21097a50a405f5e63412ecb6cf5854
Ishidres <ishidres@gmail.com>
2020-03-03 20:00:03 +0100

    added warning message; preventing possible rate limits

--- index.js
+++ index.js
@@ -44,16 +44,45 @@ client.on('message', message => {
 // checking for users to kick every 10 minutes (600 000 ms)
 setInterval(function () {
   var now = new Date().getTime();
+  var actions = 0;
 
   Object.keys(saved_users).map(function (i) {
-    if (now - saved_users[i].lastMessage >= config.maxInactivity) {
-      console.log(new Date() + ": Kicking User " + i + " due too inactivity.");
+    // Prevent ratelimits
+    if (actions >= 5)
+      return;
+
+    var diff = now - saved_users[i].lastMessage;
+    var user = client.users.get(i);
+    var channel = client.channels.get(config.warnMessagesChannel);
+
+    if (!user) {
+      user = "UserIsOffline#0000";
+    } else {
+      user = user.tag;
+    }
+
+    if (diff >= config.warnDelay && saved_users[i].warned !== true) {
+      console.log(new Date() + ": User " + i " will be kicked in " + config.warnDelay + " ms.");
+
+      if (!!channel)
+        channel.send(":warning: User " + user + " (ID: " + i + ") will be kicked in " + (config.warnDelay / 60 / 60 / 1000) + " minutes.");
+
+      saved_users[i].warned = true;
+      actions++;
+    }
+
+    if (diff >= config.maxInactivity) {
+      console.log(new Date() + ": Kicking User " + i + " due to inactivity.");
       var guild = client.guilds.get(saved_users[i].guild);
 
       guild.fetchMember(i).then(member => {
-        member.kick("Kicked due too inactivity.");
+        member.kick("Kicked due to inactivity.");
+
+        if (!!channel)
+          channel.send("🔨 " + member.user.tag + " has been kicked due to inactivity.");
       });
 
+      actions += 2;
       delete saved_users[i];
       fs.writeFileSync("./savedUsers.json", JSON.stringify(saved_users));
     }
