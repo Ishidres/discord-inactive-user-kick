eefd6574d96cb7caa31776e6314324b830e1258f
Ishidres <ishidres@gmail.com>
2020-11-27 15:20:17 +0100

    Also caching offline members

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ fetchAllMembers: true });
 
 const fs = require("fs");
 const config = require("./config.json");
